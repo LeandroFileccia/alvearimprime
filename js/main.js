2919303c9b9082d0960ce56b7ba08ad6a505d755
Leandro Fileccia <fileccialeandro@outlook.com>
2022-07-28 20:11:53 -0300

    primera entrega del proyecto final

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
-let nombreLibroA = "Atlas de Anatomia Humana Netter"
-let precioLibroA = 2000
-let stockLibroA = 50
+function Libro (nombre, precio, stock){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+}
+    
+const libro1 = new Libro ("Atlas de Anatomia Humana Netter", 2000, 50);
+const libro2 = new Libro ("Anatomia Humana Latarjet", 2800, 50);
+const libro3 = new Libro ("Histologia Texto y Atlas Ross", 3200, 50);
+const libro4 = new Libro ("Tratado de Fisiologia Medica - Guyton y Hall", 3000, 50);
 
-let nombreLibroB = "Anatomia Humana Latarjet"
-let precioLibroB = 2800
-let stockLibroB = 50
+let listaLibros = [libro1, libro2, libro3, libro4]
 
-let nombreLibroC = "Histologia Texto y Atlas Ross"
-let precioLibroC = 3200
-let stockLibroC = 50
+let nombresLibros = listaLibros.map((libro) => libro.nombre)
 
-let nombreLibroD = "Tratado de Fisiologia Medica - Guyton y Hall"
-let precioLibroD = 3000
-let stockLibroD = 50
-
-let cantidadCompras = prompt("Indique la cantidad de libros distintos que quiere comprar: \n- Atlas de Anatomia Humana Netter \n- Anatomia Humana Latarjet \n- Histologia Texto y Atlas Ross \n- Tratado de Fisiologia Medica - Guyton y Hall")
+let cantidadCompras = prompt("Indique la cantidad de libros distintos que quiere comprar: " + "\n "+nombresLibros.join("\n "))
 let precioTotal = 0;
 
 function calculoPrecio(cantidad, precio){
     precioTotal += cantidad * precio
 }
 
-function calculoStock (cantidad, stock, precio){
-    if(stock>= cantidad){
-        calculoPrecio(cantidad, precio)
-        alert("El precio total es de: $" + (cantidad* precio))
+function calculoStock (cantidad, libro){
+    if(libro.stock>= cantidad){
+        calculoPrecio(cantidad, libro.precio)
+        alert("El precio total es de: $" + (cantidad* libro.precio))
+        alert("Compraste: " + libro.nombre)
     }
     else{
         alert("No contamos con esa cantidad. Nuestro stock actual es de: " + stock + " unidades")
     }
+}
+
+function sumaIva(precio){
+    return precio * 1.21
 }
 
 for(let i =0; i< cantidadCompras; i++){
@@ -36,26 +40,21 @@ for(let i =0; i< cantidadCompras; i++){
     let cantidad1 = prompt("Ingrese la cantidad del libro que quiere comprar: ")
 
     if(compra1 == "1"){
-            calculoStock(cantidad1, stockLibroA, precioLibroA)
+            calculoStock(cantidad1, libro1)
         }
         else if(compra1 == "2"){
-            calculoStock(cantidad1, stockLibroB, precioLibroB)
+            calculoStock(cantidad1, libro2)
         }
         else if(compra1 == "3"){
-            calculoStock(cantidad1, stockLibroC, precioLibroC)
+            calculoStock(cantidad1, libro3)
         }
         else if(compra1 == "4"){
-            calculoStock(cantidad1, stockLibroD, precioLibroD)
+            calculoStock(cantidad1, libro4)
         }
         else{
             alert("No tenemos ese libro.")
         }
     }
 
-    function sumaIva (precio){
-        return precio * 1.21
-    }
-    
     let precioTotalConImpuestos = sumaIva(precioTotal)
-    
     alert("Este es el precio total final con impuestos de tu compra: $" + precioTotalConImpuestos)
