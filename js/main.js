cfe2ba966bb34902725f41e536d6db072d9ddd6e
Leandro Fileccia <fileccialeandro@outlook.com>
2022-08-04 19:17:19 -0300

    eventos

--- js/main.js
+++ js/main.js
@@ -1,76 +1,97 @@
-function Libro (nombre, precio, stock){
+function Libro(nombre, precio, stock) {
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
 }
-    
-const libro1 = new Libro ("Atlas de Anatomia Humana Netter", 2000, 50);
-const libro2 = new Libro ("Anatomia Humana Latarjet", 2800, 50);
-const libro3 = new Libro ("Histologia Texto y Atlas Ross", 3200, 50);
-const libro4 = new Libro ("Tratado de Fisiologia Medica - Guyton y Hall", 3000, 50);
+
+const libro1 = new Libro('Atlas de Anatomia Humana Netter', 2000, 50);
+const libro2 = new Libro('Anatomia Humana Latarjet', 2800, 50);
+const libro3 = new Libro('Histologia Texto y Atlas Ross', 3200, 50);
+const libro4 = new Libro('Tratado de Fisiologia Medica - Guyton y Hall', 3000, 50);
 
 let listaLibros = [libro1, libro2, libro3, libro4]
 
+for (var i = 0; i < listaLibros.length; i++) {
+    var newRow = document.getElementById('tablaDeLibros').insertRow(-1);
 
-for (const libro of listaLibros) {
-    if (libro.stock != 0) {
-        
-   
-        let card = document.createElement("div")
-        card.innerHTML =    `<h3> ${libro.nombre}</h3>
-                            <p>$ ${libro.precio}</p>
-                            <p>Unidades restantes: ${libro.stock}</p>`
-                            
-        document.body.append(card)
-    
-    } 
+    var newCell = newRow.insertCell(-1);
+    newCell.innerHTML = '<label> Titulo: </label>';
+
+    var newCell = newRow.insertCell(-1);
+    newCell.innerHTML = `<label> ${listaLibros[i].nombre} </label>`;
+
+    var newCell = newRow.insertCell(-1);
+    newCell.innerHTML = '<label> Precio: </label>';
+
+    var newCell = newRow.insertCell(-1);
+    newCell.innerHTML = `<label> ${listaLibros[i].precio} </label>`;
+
+    newCell = newRow.insertCell(-1);
+    newCell.innerHTML = '<label>Cantidad:</label>';
+
+    newCell = newRow.insertCell(-1);
+    newCell.innerHTML = `<input id='${listaLibros[i].nombre}' type='number' name='cantidad'  value='0' />`;
+
 }
 
+var botonAgregar = document.getElementById('comprarLibro');
+botonAgregar.addEventListener('click', comprarLibro)
 
-let nombresLibros = listaLibros.map((libro) => libro.nombre)
+function comprarLibro() {
+    var total = 0;
 
-let cantidadCompras = prompt("Indique la cantidad de libros distintos que quiere comprar: " + "\n "+nombresLibros.join("\n "))
-let precioTotal = 0;
+    if (document.getElementById('tablaCompraDeLibros')) {
+        document.getElementById('tablaCompraDeLibros').remove()
+    };
+    var table = document.createElement('table');
+    table.id = 'tablaCompraDeLibros';
 
-function calculoPrecio(cantidad, precio){
-    precioTotal += cantidad * precio
-}
+    for (var i = 0; i < listaLibros.length; i++) {
 
-function calculoStock (cantidad, libro){
-    if(libro.stock>= cantidad){
-        calculoPrecio(cantidad, libro.precio)
-        alert("El precio total es de: $" + (cantidad* libro.precio))
-        alert("Compraste: " + libro.nombre)
+        let cantidad = document.getElementById(`${listaLibros[i].nombre}`).value;
+
+        var newRow = table.insertRow(-1);
+
+        var newCell = newRow.insertCell(-1);
+        newCell.innerHTML = '<label> Titulo: </label>';
+
+        var newCell = newRow.insertCell(-1);
+        newCell.innerHTML = `<label> ${listaLibros[i].nombre} </label>`;
+
+        var newCell = newRow.insertCell(-1);
+        newCell.innerHTML = '<label> Precio: </label>';
+
+        var newCell = newRow.insertCell(-1);
+        newCell.innerHTML = `<label> ${listaLibros[i].precio} </label>`;
+
+        newCell = newRow.insertCell(-1);
+        newCell.innerHTML = '<label>Cantidad:</label>';
+
+        if (listaLibros[i].stock < cantidad) {
+            newCell = newRow.insertCell(-1);
+            newCell.innerHTML = `<label>No tenemos sufiente stock, te debemos ${cantidad - listaLibros[i].stock} libros te puedo vender ${listaLibros[i].stock}</label>`;
+            cantidad = listaLibros[i].stock
+        } else {
+            var newCell = newRow.insertCell(-1);
+            newCell.innerHTML = `<label> ${cantidad} </label>`;
+        }
+
+        newCell = newRow.insertCell(-1);
+        newCell.innerHTML = '<label>Total:</label>';
+
+        var newCell = newRow.insertCell(-1);
+        newCell.innerHTML = `<label> ${cantidad * listaLibros[i].precio} </label>`;
+
+        total += cantidad * listaLibros[i].precio;
     }
-    else{
-        alert(`No contamos con esa cantidad. Nuestro stock actual es de:  ${libro.stock} unidades`)
-    }
+
+    var newRow = table.insertRow(-1);
+    newCell = newRow.insertCell(-1);
+    newCell.innerHTML = '<label>Total:</label>';
+
+    var newCell = newRow.insertCell(-1);
+    newCell.innerHTML = `<label> ${total} </label>`;
+
+    document.body.append(table);
+
 }
-
-function sumaIva(precio){
-    return precio * 1.21
-}
-
-for(let i =0; i< cantidadCompras; i++){
-    let compra1 = prompt("Ingrese la opcion del libro que quiere comprar : \n1- Atlas de Anatomia Humana Netter \n2- Anatomia Humana Latarjet \n3- Histologia Texto y Atlas Ross \n4- Tratado de Fisiologia Medica - Guyton y Hall")
-    let cantidad1 = prompt("Ingrese la cantidad del libro que quiere comprar: ")
-
-    if(compra1 == "1"){
-            calculoStock(cantidad1, libro1)
-        }
-        else if(compra1 == "2"){
-            calculoStock(cantidad1, libro2)
-        }
-        else if(compra1 == "3"){
-            calculoStock(cantidad1, libro3)
-        }
-        else if(compra1 == "4"){
-            calculoStock(cantidad1, libro4)
-        }
-        else{
-            alert("No tenemos ese libro.")
-        }
-    }
-
-    let precioTotalConImpuestos = sumaIva(precioTotal)
-    alert("Este es el precio total final con impuestos de tu compra: $" + precioTotalConImpuestos)
