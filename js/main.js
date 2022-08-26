e815dfbcc36bf538aa33e790b614a7ce1497c01a
Leandro Fileccia <fileccialeandro@outlook.com>
2022-08-25 21:54:32 -0300

    Archivo Json agregado

--- js/main.js
+++ js/main.js
@@ -1,49 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+fetch('../data.json')
+    .then((resp) => resp.json())
+    .then((data) => inicio(data))
 
-    // Variables
-
-    //let baseDeDatos = [];
-
+function inicio(listaLibros) {
+    
     let carrito = [];
     const DOMitems = document.getElementById('items');
     const DOMcarrito = document.querySelector('#carrito');
     const DOMtotal = document.querySelector('#total');
     const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
-
-    const listaLibros = [{
-            id: 1,
-            nombre: "Atlas deAnatomia Humana Netter",
-            precio: 2000,
-            stock: 50,
-            img: 'galeria/libros/netter.jpg'
-        },
-        {
-            id: 2,
-            nombre: "Anatomia Humana Latarjet",
-            precio: 2800,
-            stock: 50,
-            img: 'galeria/libros/latarjet.jpg'
-        },
-        {
-            id: 3,
-            nombre: "Histologia Texto y Atlas Ross",
-            precio: 3200,
-            stock: 50,
-            img: 'galeria/libros/ross.jpg'
-        },
-        {
-            id: 4,
-            nombre: "Tratado de Fisiologia Medica - Guyton y Hall",
-            precio: 3000,
-            stock: 50,
-            img: 'galeria/libros/guyton.jpg'
-        },
-    ]
-
     // Funciones
     function renderizarProductos() {
         listaLibros.forEach((info) => {
+
             // Estructura
             const miNodo = document.createElement('div');
             miNodo.classList.add('card', 'col-sm-4');
@@ -80,42 +50,33 @@ document.addEventListener('DOMContentLoaded', () => {
             miNodoCardBody.append(miNodoBoton);
             miNodo.append(miNodoCardBody);
             DOMitems.append(miNodo);
+
         });
     }
 
-    /**
-     * Evento para añadir un producto al carrito de la compra
-     */
+
+    // Evento para añadir un producto al carrito de la compra
+
     function agregarProductoAlCarrito(e) {
-        // Anyadimos el Nodo a nuestro carrito
         carrito.push(e.target.getAttribute('marcador'))
-        // Actualizamos el carrito 
         renderizarCarrito();
-        // Actualizamos el LocalStorage
         guardarCarritoEnLocalStorage();
     }
 
-    /**
-     * Dibuja todos los productos guardados en el carrito
-     */
+
+    // Aparece los productos guardados en el carrito
+
     function renderizarCarrito() {
-        // Vaciamos todo el html
         DOMcarrito.innerText = '';
-        // Quitamos los duplicados
         const carritoSinDuplicados = [...new Set(carrito)];
-        // Generamos los Nodos a partir de carrito
         carritoSinDuplicados.forEach((item) => {
-            // Obtenemos el item que necesitamos de la variable base de datos
             const miItem = listaLibros.filter((itemBaseDatos) => {
-                // ¿Coincide las id? Solo puede existir un caso
                 return itemBaseDatos.id === parseInt(item);
             });
-            // Cuenta el número de veces que se repite el producto
             const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
                 return itemId === item ? total += 1 : total;
             }, 0);
-            // Creamos el nodo del item del carrito
+
             const miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
             miNodo.innerText = `${numeroUnidadesItem} x ${miItem[0].nombre} - $${miItem[0].precio}`;
@@ -126,59 +87,45 @@ document.addEventListener('DOMContentLoaded', () => {
             miBoton.style.marginLeft = '1rem';
             miBoton.dataset.item = item;
             miBoton.addEventListener('click', borrarItemCarrito);
-            // Mezclamos nodos
+
             miNodo.appendChild(miBoton);
             DOMcarrito.appendChild(miNodo);
-
         });
-        // Renderizamos el precio total en el HTML
+
         DOMtotal.innerText = `$${calcularTotal()}`;
 
     }
 
-    /**
-     * Evento para borrar un elemento del carrito
-     */
+
+    // Evento para borrar un elemento del carrito
+
     function borrarItemCarrito(e) {
-        // Obtenemos el producto ID que hay en el boton pulsado
         const id = e.target.dataset.item;
-        // Borramos todos los productos
         carrito = carrito.filter((carritoId) => {
             return carritoId !== id;
         });
-        // volvemos a renderizar
         renderizarCarrito();
-        // Actualizamos el LocalStorage
         guardarCarritoEnLocalStorage();
 
     }
 
-    /**
-     * Calcula el precio total teniendo en cuenta los productos repetidos
-     */
+    // Calcula el precio total teniendo en cuenta los productos repetidos
+
     function calcularTotal() {
-        // Recorremos el array del carrito 
         return carrito.reduce((total, item) => {
-            // De cada elemento obtenemos su precio
             const miItem = listaLibros.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
-            // Los sumamos al total
-
             return total + miItem[0].precio;
         }, 0).toFixed(2);
 
     }
 
-    /**
-     * Varia el carrito y vuelve a dibujarlo
-     */
+    // Varia el carrito y vuelve a dibujarlo
+
     function vaciarCarrito() {
-        // Limpiamos los productos guardados
         carrito = [];
-        // Renderizamos los cambios
         renderizarCarrito();
-        // Borra LocalStorage
         localStorage.removeItem('carrito');
 
     }
@@ -187,67 +134,53 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('carrito', JSON.stringify(carrito));
     }
 
+    function guardarCompraEnLocalStorage() {
+        let compra = JSON.parse(localStorage.getItem('compra'));
+        console.log(compra)
+        if (compra) {
+            compra.push({
+                compraNumero: compra[compra.length - 1].compraNumero + 1,
+                compra: carrito,
+                total: calcularTotal()
+            })
+            localStorage.setItem('compra', JSON.stringify(compra));
+        } else {
+            localStorage.setItem('compra', JSON.stringify(
+                [{
+                    compraNumero: 0,
+                    compra: carrito,
+                    total: calcularTotal()
+                }]
+            ));
+        }
+    }
+
     function cargarCarritoDeLocalStorage() {
-        // ¿Existe un carrito previo guardado en LocalStorage?
         if (localStorage.getItem('carrito') !== null) {
-            // Carga la información
             carrito = JSON.parse(localStorage.getItem('carrito'));
         }
     }
 
-    // Eventos
+
     DOMbotonVaciar.addEventListener('click', vaciarCarrito);
-
-
     let botonComprar = document.getElementById('boton-comprar');
     if (botonComprar.addEventListener('click', comprarLibros)) {
         comprarLibros();
     }
 
-
     function comprarLibros() {
         Swal.fire({
-            tittle: 'Aprobado!',
+            title: 'Aprobado!',
             text: 'Felicitaciones por su compra!',
             icon: 'success'
         })
-
+        guardarCompraEnLocalStorage();
     }
 
-    // Inicio
     cargarCarritoDeLocalStorage();
     renderizarProductos();
     renderizarCarrito();
 
-});
-
-// Para Oferta o Publicidad
-/* setTimeout(() =>{
-    Swal.fire({
-        icon: 'success',
-        text: "Alerta!",
-        timer: 2000
-    })
-}, 3000) */
 
 
-// Para confirmar vaciar el carrito o la compra de los productos
-/* function vaciarCarrito() {
-    Swal.fire({
-        icon: 'warning',
-        text: "Estas seguro de vaciar el carrito?",
-        showDenyButtonText: true,
-        confirmButtonText: 'Vaciar',
-        denyButtonText: 'No Vaciar',
-    }).then(() => {
-        if (result.isConfirmed) {
-            Swal.fire('Vaciamos el carrito!', '', 'success')
-            toDoList.innerHTML = ""
-            localStorage.clear()
-        }
-        else if(result.isDismissed){
-            Swal.fire('No supimos que hacer, asique no hicimos nada', '', 'succcess')
-        }
-    })
-
-} */
+}
